ce71cd845104e7d3e78fa78c3f556b8e6eb5c623
ntsum <natsukajiwaram@gmail.com>
2019-09-09 12:35:17 -0400

    styling

--- App.js
+++ App.js
@@ -4,8 +4,22 @@ import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 export default function App() {
   return (
     <View style={{ padding: 50 }}>
-      <View>
-        <TextInput placeholder="Course Goalz" style={{ border: "black" }} />
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignContent: "center"
+        }}
+      >
+        <TextInput
+          placeholder="Course Goalz"
+          style={{
+            width: 200,
+            borderColor: "black",
+            borderWidth: 1,
+            padding: 10
+          }}
+        />
         <Button title="Add" />
       </View>
     </View>
